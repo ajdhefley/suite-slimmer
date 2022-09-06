2b22d5ed4e1f3fd65ba71ff4d35ff895cb79d9e1
Ashton Hefley <ajdhefley@gmail.com>
2022-09-06 00:08:06 -0500

    Add ability to set up explicit mock

--- src/test-mock-mapper.ts
+++ src/test-mock-mapper.ts
@@ -1,11 +1,14 @@
-import { MockType, MockOf } from './test-mock';
-import { mockService } from './test-mock';
+import { MockType, MockOf, mockService } from './test-mock';
 
 export class TestMockMapper {
     private mocks = {};
 
     add<TMock>(type: MockType<TMock>) {
         this.mocks[type.name] = mockService(type);
+    }
+
+    addExplicit<TMock>(type: MockType<TMock>, value: any) {
+        this.mocks[type.name] = value;
     }
 
     get<TMock>(serviceType: MockType<TMock>): MockOf<TMock> {
